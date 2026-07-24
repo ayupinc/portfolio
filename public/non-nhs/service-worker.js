84b406f4cf2e108bd62a7035fd160ed37b29b8ca
Stephen Clinton <steve@ayup.uk>
2026-07-24 07:43:59 +0100

    standardise app typography to sans serif

--- public/non-nhs/service-worker.js
+++ public/non-nhs/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "take-home-v4";
+const CACHE = "take-home-v5";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "../assets/maple-leaf.png"];
 
 self.addEventListener("install", (event) => {
